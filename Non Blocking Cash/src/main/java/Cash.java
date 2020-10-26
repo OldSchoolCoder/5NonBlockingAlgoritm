ae3423e23d4f94b5176e6ec7ac2c7ccc71f9b441
dima <test@sd.com>
2020-10-26 20:41:30 +0500

    1. Неблокирующий кеш [#333762]

--- Non Blocking Cash/src/main/java/Cash.java
+++ Non Blocking Cash/src/main/java/Cash.java
@@ -1,23 +1,20 @@
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Cash {
     private final ConcurrentHashMap<Integer, Base> map = new ConcurrentHashMap<>();
-    private final AtomicInteger id = new AtomicInteger();
 
     public void add(Base base) {
-        map.put(id.incrementAndGet(), base);
+        map.put(base.id, base);
     }
 
     public void update(Base base) throws OptimisticException {
-        if (map.containsKey(base.id)) {
-            map.computeIfPresent(base.id, (key, value) -> {
-                value.version++;
-                return value;
-            });
-        } else {
+        if (!map.containsKey(base.id)) {
             throw new OptimisticException("Mismatch version!");
         }
+        map.computeIfPresent(base.id, (key, value) -> {
+            value.version++;
+            return value;
+        });
     }
 
     public void delete(Base base) {
